3a79427c241fb6b44e500102d4992fdfc6457f80
Iwona Daleka <iwona.daleka@gmail.com>
2025-11-14 15:29:49 +0100

    Added Pokemons' as objects, removed test JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-alert('Hello, world!');
-let favoriteFood = 'pizza';
-document.write('My favorite food is ' + favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: 2, types ['grass', 'poison']},
+    { name: 'Charmander', height: 2, types: ['fire']},
+    { name: 'Squirtle', height: 1, types: ['water']},
+    { name: 'Caterpie', height: 6, types: ['bug']},
+    { name: 'Butterfree', height: 3, types: ['bug', 'flying']},
+    { name: 'Pidgey', height: 1, types: ['normal', 'flying']},
+    { name: 'Rattata', height: 1, types: ['normal']},
+    { name: 'Pikachu', height: 1, types: ['electric']},
+    { name: 'Jigglypuff', height: 1, types: ['normal', 'fairy']},
+    { name: 'Zubat', height: 2, types: ['poison', 'flying']}
+]
