bf421df5ac833dcc35ab4c3dd05a651bd070a621
Iwona Daleka <iwona.daleka@gmail.com>
2025-11-17 11:55:15 +0100

    Added loop that prints all pokemons' names and their height on the index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 2, types ['grass', 'poison']},
+    { name: 'Bulbasaur', height: 2, types: ['grass', 'poison']},
     { name: 'Charmander', height: 2, types: ['fire']},
     { name: 'Squirtle', height: 1, types: ['water']},
     { name: 'Caterpie', height: 6, types: ['bug']},
@@ -10,3 +10,11 @@ let pokemonList = [
     { name: 'Jigglypuff', height: 1, types: ['normal', 'fairy']},
     { name: 'Zubat', height: 2, types: ['poison', 'flying']}
 ]
+// Printing get all Pokemon names
+for (i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    if (pokemonList[i].height >= 3) {
+        document.write(' - This Pokemon is a big Pokemon!');
+    }
+    document.write('<br>');
+}
