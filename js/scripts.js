33eaeba2df786ba55da1cdfc490007108cb61deb
Iwona Daleka <iwona.daleka@gmail.com>
2025-11-20 14:09:31 +0100

    Encapsulated repository within IIFE, transformed forLoop into a forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,40 @@
-// Pokemon data array
-let pokemonList = [
-    { name: 'Bulbasaur', height: 2, types: ['grass', 'poison'] },
-    { name: 'Charmander', height: 2, types: ['fire'] },
-    { name: 'Squirtle', height: 1, types: ['water'] },
-    { name: 'Caterpie', height: 6, types: ['bug'] },
-    { name: 'Butterfree', height: 3, types: ['bug', 'flying'] },
-    { name: 'Pidgey', height: 1, types: ['normal', 'flying'] },
-    { name: 'Rattata', height: 1, types: ['normal'] },
-    { name: 'Pikachu', height: 1, types: ['electric'] },
-    { name: 'Jigglypuff', height: 1, types: ['normal', 'fairy'] },
-    { name: 'Zubat', height: 2, types: ['poison', 'flying'] }
-]
+// IIFE encapsulating the Pokemon repository
+let pokemonRepository = (function () {
+    // Pokemon data array
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 2, types: ['grass', 'poison'] },
+        { name: 'Charmander', height: 2, types: ['fire'] },
+        { name: 'Squirtle', height: 1, types: ['water'] },
+        { name: 'Caterpie', height: 6, types: ['bug'] },
+        { name: 'Butterfree', height: 3, types: ['bug', 'flying'] },
+        { name: 'Pidgey', height: 1, types: ['normal', 'flying'] },
+        { name: 'Rattata', height: 1, types: ['normal'] },
+        { name: 'Pikachu', height: 1, types: ['electric'] },
+        { name: 'Jigglypuff', height: 1, types: ['normal', 'fairy'] },
+        { name: 'Zubat', height: 2, types: ['poison', 'flying'] }
+    ];
+
+    // Function to get all Pokemon
+    function getAll() {
+        return pokemonList;
+    }
+
+    // function to add a new Pokemon
+    function add(item) {
+        pokemonList.push(item);
+    }
+    // Return statement of the whole IIFE
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
 
 // Function to print Pokemon names and height details
-function printArrayDetails(list) {
-    // Printing get all Pokemon names
-    for (i = 0; i < list.length; i++) {
-        document.write(list[i].name + ' (height: ' + list[i].height + ')');
-        if (list[i].height >= 3) {
-            document.write(' - This Pokemon is a big Pokemon!');
-        }
-        document.write('<br>');
+pokemonRepository.getAll().forEach(function (list) {
+    document.write(list.name + ' (height: ' + list.height + ')');
+    if (list.height >= 3) {
+        document.write(' - This Pokemon is a big Pokemon!');
     }
-}
-
-printArrayDetails(pokemonList);
+    document.write('<br>');
+});
